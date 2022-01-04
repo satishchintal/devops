a08a5d182c18cf4bd60383141be022868c256784
satishchintal <satishchintal99@gmail.com>
2022-01-04 16:17:11 +0530

    myproject

---
+++ satish/src/main/java/com/su/satish/SatishApplication.java
@@ -0,0 +1,13 @@
+package com.su.satish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SatishApplication.class, args);
+	}
+
+}
---
+++ satish/src/main/java/com/su/satish/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.su.satish;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SatishApplication.class);
+	}
+
+}
