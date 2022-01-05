ff727f3eed5c22e5ade169da1028a6f49af94913
satishchintal <satishchintal99@gmail.com>
2022-01-05 16:38:33 +0530

    shiva

---
+++ chintal/src/main/java/satish/chintal/ChintalApplication.java
@@ -0,0 +1,13 @@
+package satish.chintal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChintalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChintalApplication.class, args);
+	}
+
+}
---
+++ chintal/src/test/java/satish/chintal/ChintalApplicationTests.java
@@ -0,0 +1,13 @@
+package satish.chintal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChintalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
